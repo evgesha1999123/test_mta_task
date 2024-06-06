481d7022f98622d5a488d95f97baa9de9f7f920d
evgesha <evgesha.22013@gmail.com>
2024-06-06 23:15:25 +0300

    done

---
+++ resources/myserver/salutations.lua
@@ -0,0 +1,32 @@
+function salutations_sir ( )
+	local player_name = getPlayerName ( source )
+	outputChatBox ( "Добро пожаловать, " .. player_name .."!" )
+end
+
+function rgb_message( player, cmd, r, g, b, text )  
+	local min_color_value = 0
+	local max_color_value = 255
+
+		if cmd == 'chatmessage' then
+			r = tonumber(r)
+			g = tonumber(g)
+			b = tonumber(b)
+
+			if (r == nil) or (g == nil) or (b == nil) then 
+				outputChatBox('[использование] /chatmessage [r] [g] [b] in range from 0 to 255 [text]', player)
+			end
+
+			if (r ~= nil) and (g ~= nil) and (b ~= nil) then
+				if (r >= 0 and r <= 255) and (g >= 0 and g <= 255) and (b >= 0 and b <= 255) and (text ~= nil) then
+					outputChatBox(text, player, r, g, b, true)
+				else outputChatBox('[использование] /chatmessage [r] [g] [b] in range from 0 to 255 [text]', player)
+				end
+			end
+
+		end
+end
+
+addEventHandler ( "onPlayerJoin", root, salutations_sir )
+
+addCommandHandler ( "chatmessage", rgb_message ) 
+--Спасибо за внимание =)
